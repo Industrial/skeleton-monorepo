6581ccb6947b191a07144e1477ed9d7a2b573f56
Tom Wieland <tom.wieland@gmail.com>
2023-08-30 03:00:21 +0200

    refactor: flake, pnpm-lock and tsconfig

--- flake.nix
+++ flake.nix
@@ -36,7 +36,18 @@
           nodejs_20.pkgs.eslint
           nodejs_20.pkgs.pnpm
           nodejs_20.pkgs.typescript
+
+          # Prisma
+          openssl
+          nodejs_20.pkgs.prisma
         ];
+        shellHook = with pkgs; ''
+          export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+        '';
       };
     }
   ));
